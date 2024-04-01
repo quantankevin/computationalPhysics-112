a3ffdcc8d40bfec296e1696298a8526ad6cb7a42
quantankevin <kevin980034@gmail.com>
2024-04-01 17:07:30 +0800

    4/1

--- project2/nbody/particles.py
+++ project2/nbody/particles.py
@@ -6,5 +6,11 @@ class Particles:
     """
     Particle class to store particle properties
     """
+    def __init__(self, N:int=100):
+        self.nparticles = N
+        self._masses = np.ones((N,1))
+        self._positions = np.zeros((N,3))
+        self._velocities = np.zeros((N,3))
+        self._accelerations = np.zeros((N,3))
     pass
 
